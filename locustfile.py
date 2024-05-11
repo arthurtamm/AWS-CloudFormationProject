34909e0375e1313e82e17db55410574da13d7093
arthurtamm <arthurmtamm@gmail.com>
2024-05-11 19:05:21 -0300

    yaml, locust e estimativa

---
+++ locustfile.py
@@ -0,0 +1,22 @@
+from locust import HttpUser, task, between, TaskSet
+
+class UserBehavior(TaskSet):
+    @task(2)
+    def view_main_page(self):
+        # Acessar a página principal
+        self.client.get("/")
+
+    @task(3)
+    def create_user(self):
+        # Criar um usuário fictício
+        self.client.get("/create_user", params={"user": "testuser"})
+
+    @task(1)
+    def list_users(self):
+        # Listar usuários
+        self.client.get("/list_users")
+
+class WebsiteUser(HttpUser):
+    tasks = [UserBehavior]
+    wait_time = between(1, 10)
+    host = "stack2-MyLoa-dGxUFuBZAX86-714783549.us-east-1.elb.amazonaws.com"
